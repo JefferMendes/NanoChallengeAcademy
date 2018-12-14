fb6d2ab2c2a7b909ef50411cbfbfd68f67c9cd60
Jafer Daltro Pompeu Júnior <jafer.junior@10-45-48-43.0200.ifce.br.iosda.org>
2018-12-14 15:10:33 -0300

    criado arquivo Searh.swift

---
+++ NanoChallengeAcademy/Searches.swift
@@ -0,0 +1,19 @@
+//
+//  Searches.swift
+//  NanoChallengeAcademy
+//
+//  Created by Jafer Daltro Pompeu Júnior on 14/12/2018.
+//  Copyright © 2018 Emanuel Jefferson Mendes Vasconcelos. All rights reserved.
+//
+
+import Foundation
+import CoreData
+struct Searches {
+    let contexto: NSManagedObjectContext
+    
+    init(contexto: NSManagedObjectContext) {
+        self.contexto = contexto
+    }
+    
+    
+}
